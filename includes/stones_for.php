f40f720cf760e6e80a4037208f8cc03b57d81663
unknown <anzarsh@mail.ru>
2017-01-24 16:02:31 +0300

    add 1

--- includes/stones_for.php
+++ includes/stones_for.php
@@ -1,14 +1,15 @@
-<section class="section">
+<section class="section section_padding section-db-stone">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-12">
-				<h2 class="db-stone-h2">
+		<div class="col-md-4"></div>
+			<div class="col-md-8">
+				<div class="db-stone-h2">
 					<span class="db-stone-one">
 					Согласно мнению многих покупателей: <br>
 					</span>
 					Камни для виски Sheriff Premium - <br>
 					подарок с <span class="db-stone-two">мужским характером!</span>
-				</h2>
+				</div>
 				<a href="#" class="button-1">заказать идеальный подарок</a>
 			</div>
 		</div>
