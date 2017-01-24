b19e4920b62a0fb9c805f8377ba1fb9fb31c2de9
unknown <anzarsh@mail.ru>
2017-01-24 15:02:53 +0300

    add 1

--- includes/stones_for.php
+++ includes/stones_for.php
@@ -2,7 +2,14 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<h2></h2>
+				<h2 class="db-stone-h2">
+					<span class="db-stone-one">
+					Согласно мнению многих покупателей: <br>
+					</span>
+					Камни для виски Sheriff Premium - <br>
+					подарок с <span class="db-stone-two">мужским характером!</span>
+				</h2>
+				<a href="#" class="button-1">заказать идеальный подарок</a>
 			</div>
 		</div>
 	</div>
