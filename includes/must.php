62aa3ffe1bfff6ff7b0238649a4cf61fae682f0b
engine77 <zaur.ka.00@mail.ru>
2017-01-24 11:03:18 +0300

    24

--- includes/must.php
+++ includes/must.php
@@ -1,9 +1,19 @@
 <section class="section">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<h2></h2>
+	<div class="df-container">
+		<h2 class="df-cap1">Обязательно прочитайте этот текст до конца!</h2>
+		<p class="df-cap2">Мужчины мало чем отличаются от детей – любят сюрпризы, приятности, новенькие штучки и внимание</p>
+		
+		<div class="df-must-all df-must-all_view">		
+			<div class="df-must df-must_view">
+				<div class="df-must__img df-must__img_view">
+					<img src="img/df-img1.png" alt="">
+				</div>
+				<p class="df-must__txt df-must__txt_view">Камни для виски Sheriff Premium – <strong>подарок специально для мужчин!</strong> Это <strong>оригинальный и неожиданный</strong> подарок – то, что вашему мужчине в обозримом будущем, скорее всего, никто не подарит.</p>
 			</div>
 		</div>
+
+
+
+
 	</div>
 </section>
