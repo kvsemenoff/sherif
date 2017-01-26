95d2db8480e1e631d374dc3f51c8c8ee83aafcb9
engine77 <zaur.ka.00@mail.ru>
2017-01-26 16:49:48 +0300

    25

--- includes/popups.php
+++ includes/popups.php
@@ -1,1 +1,76 @@
-aaaaaaaaaaaaaaaaaaaa
+<div class="df-messege">
+	<h2>Сообщение успешно отправленно!</h2>
+	<span>Менеджер свяжется с вами в ближайшее рабочее время.</span>
+</div>
+
+<div class="df-call">
+	<div class="df-call__L">
+		<h3>Камни для виски Sheriff Premium</h3>
+		<span class="dfspan11">Количество:</span>
+		<div class="df-buy-product">
+			<span class="df-number">
+				<span class="dfbutton left">-</span>
+				<input type="text" value="5" disabled="disabled">  
+				<span class="dfbutton right">+</span>
+			</span>			
+		</div>
+		<span class="dfspan11">Стоимость заказа: <strong>1 990</strong> руб.</span>
+		<div class="df-call__img">
+			<img src="img/df-call.jpg" alt="" class="img-responsive">
+		</div>
+	</div>
+	<div class="df-call__R">
+		<form action="">
+			<span class="df-call-span">
+				Оставьте свой телефон, менеджер перезвонит <br> и решит все вопросы по оформлению заказа
+			</span>
+			<input type="text" placeholder="Телефон...">
+			<a href="#" class="df-linkpopup">Позвоните мне</a>
+			<span class="df-call-span2">Или заполните форму заказа</span>
+
+			<input type="text" placeholder="Имя и фамилия...">
+			<input type="text" placeholder="Телефон...">
+			<input type="text" placeholder="E-mail...">
+
+			<div  class="az-select az-margin-bottom10">
+					<select name="" id="">
+						<option value="1">Способ доставки</option>
+						<option value="2">1</option>
+						<option value="3">2</option>
+						<option value="4">3</option>
+						<option value="5">4</option>
+						
+					</select>
+				</div>
+
+			<input type="text" placeholder="Адрес доставки...">
+			<a href="#" class="df-link df-mustlink df-poplink">оформить заказ</a>
+		</form>
+	</div>
+</div>
+
+<div class="df-contact">
+	<div class="df-cont">
+		<h2>Оставьте ваши контакты</h2>
+		<span>И мы перезвоним вам в ближайшее время</span>
+	</div>
+	<div class="df-call__R df-inputscont">
+		<input type="text" placeholder="Имя и фамилия...">
+		<input type="text" placeholder="Телефон...">
+		<input type="text" placeholder="E-mail...">
+	</div>
+	<a href="#" class="df-link df-mustlink df-poplink">Заказать звонок</a>
+</div>
+<div class="df-contact df-question">
+	<div class="df-cont">
+		<h2>Оставьте ваш вопрос</h2>
+		<span>И мы перезвоним вам в ближайшее время</span>
+	</div>
+	<div class="df-call__R df-inputscont">
+		<input type="text" placeholder="Имя и фамилия...">
+		<input type="text" placeholder="Телефон...">
+		<input type="text" placeholder="E-mail...">
+		<textarea name="" id="" cols="53" rows="4" placeholder="Вопрос..."></textarea>
+	</div>
+	<a href="#" class="df-link df-mustlink df-poplink">Заказать вопрос</a>
+</div>
--- js/common.js
+++ js/common.js
@@ -104,6 +104,29 @@ $(document).ready(function(){
         navigation_scroll();
     });
 });
+
+
+
+   $(".dfbutton").on("click", function() {
+
+    var $button = $(this);
+    var oldValue = $button.parent().find("input").val();  
+
+    if ($button.text() == "+") {
+      var newVal = parseFloat(oldValue) + 1;
+    }  
+    else {    
+      if (oldValue > 1) {
+        var newVal = parseFloat(oldValue) - 1;
+       } else {
+        newVal = 1;
+      }
+    }
+
+  $button.parent().find("input").val(newVal);
+
+});
+ 
 });
 
 
