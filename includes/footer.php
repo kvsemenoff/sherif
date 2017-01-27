b75620280ecd3a31e9fc82fc01fd141258a4fc36
Berzeg07 <berezg86@gmail.com>
2017-01-27 16:04:18 +0300

    29.14

--- includes/footer.php
+++ includes/footer.php
@@ -3,6 +3,14 @@
 		<div class="dd-footer-logo-m">
 			<img src="img/lf.png" alt="">
 		</div>
+		<div class="dd-footer-logo-m2">
+			<img src="img/fb.png" alt="">
+		</div>
+		<!-- <div class="dd-name-f">
+			<span class="dd-mobile-title">Интернет-магазин камней для виски </span><br>
+			<span class="dd-mobile-title2">Sheriff Premium  <span>с</span> доставкой по России</span>
+		</div> -->
+
 		<div class="dd-time-of-work-f">
 			<span class="dd-span-phone">8 (567)<span> 876 87 98</span></span><br>
 			<div class="dd-txt-f">
@@ -10,36 +18,45 @@
 					<span class="dd-2">до</span><span class="dd-3"> 19:00,</span><br><span class="dd-2"> СБ.ВС с</span> <span class="dd-3 dd-3-2">11:00</span> <span class="dd-2">до</span> <span class="dd-3 dd-3-2"> 15:00
 				</span>
 			</div>
+			<div class="dd-txt-f2">
+				<span class="dd-1">Время работы:</span><span class="dd-2"> ПН-ПТ с</span><span class="dd-3"> 10:00</span><br>
+				<span class="dd-2">до</span><span class="dd-3"> 19:00</span><span class="dd-2">, СБ.ВС с</span> <span class="dd-3 dd-3-2">11:00</span> <span class="dd-2">до</span> <span class="dd-3 dd-3-2"> 15:00</span>
+			</div>
 		</div>
-		<div class="clearfix"></div>
-		<div class="dd-mobile-title-wrap dd-mobile-title-wrap2">
+
+		<div class="clearfix clear-none"></div>
+		<div class="dd-g">
 			<span class="dd-mobile-title">Интернет-магазин камней для виски </span><br>
 			<span class="dd-mobile-title2">Sheriff Premium  <span>с</span> доставкой по России</span>
 		</div>
 		<div class="dd-social-wrap">
 			<div class="dd-soc-slogan">
-				<span>Расскажите о камнях друзьям:</span>
+				<span class="sf">Расскажите о камнях друзьям:</span>
 			</div>
 			<div class="dd-icons">
 				<ul>
-					<li><div class="box-ico">
-						
-					</div></li>
-					<li><div class="box-ico">
-						
-					</div></li>
-					<li><div class="box-ico">
-						
-					</div></li>
-					<li><div class="box-ico">
-						
-					</div></li>
-					<li><div class="box-ico">
-						
-					</div></li>
+					<li><a href="#"><div class="box-ico"><i class="fa fa-vk" aria-hidden="true"></i>
+					</div></a></li>
+					<li><a href="#"><div class="box-ico"><i class="fa fa-google-plus" aria-hidden="true"></i>
+					</div></a></li>
+					<li><a href="#"><div class="box-ico"><i class="fa fa-vimeo" aria-hidden="true"></i>
+					</div></a></li>
+					<li><a href="#"><div class="box-ico"><i class="fa fa-twitter" aria-hidden="true"></i>
+					</div></a></li>
+					<li><a href="#"><div class="box-ico"><i class="fa fa-facebook" aria-hidden="true"></i>
+					</div></a></li>
 				</ul>
 			</div>	
+			<div class="dd-soc-slogan">
+				<a class="sf sf2" href="#">Политика конфидициальности</a>
+			</div>
 		</div>
+		<!-- <div class="clearfix clear-block"></div> -->
+		<div class="dd-big-txt-f">
+			<span>© 2016-2017 Все права защищены законом о защите авторских прав.  Полное или частичное копирование материалов запрещено. Ответственность за несанкционированное копирование и коммерческое использование любых материалов несут лица и организации, неправомочно использовавшие опубликованные на данном сайте материалы.
+			</span>
+		</div>
+		<div class="clearfix"></div>	
 	</div>
 </section>
 
