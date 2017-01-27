c4b797393799828dd139acb9d1784fe83566178a
unknown <anzarsh@mail.ru>
2017-01-27 09:14:15 +0300

    add 1

--- includes/select_deliver.php
+++ includes/select_deliver.php
@@ -5,17 +5,20 @@
 				<h2 class="db-select-h2">Выберите удобный вам способ доставки и оплаты</h2>
 				<br>
 				<div class="db-deliver">
-					<script src="https://delivery.yandex.ru/widget/widgetJsLoader?dataTagID=af1f2aa717f2b931c7cceaca08ad1bd8" charset="utf-8"  class="db-deliver-one"></script>
-					<msw id="a5fccd1f979ef97963355f88df78059d" class="yd-widget-container"></msw>
+					<script src="https://delivery.yandex.ru/widget/widgetJsLoader?dataTagID=af1f2aa717f2b931c7cceaca08ad1bd8" charset="utf-8" width="100%"></script>
+					<msw id="a5fccd1f979ef97963355f88df78059d" class="yd-widget-container db-widget"></msw>
 				</div>
 				<p  class="db-select-one">Оплата при получении курьеру, в пункте самовывоза или в ближайшем к вам отделении Почты.</p>
 				<span class="db-select-span">При заказе сразу 2 комплектов подарочных камней для виски Sheriff Premium, доставка по России бесплатно!</span>		
 				<p class="db-select-two">Стоимость курьерской доставки по Москве составляет 290р. и осуществляется на следующий день после оформления заказа. Возможна срочная доставка в день заказа, стоимость составляет 490р. Доставка осуществляется с понедельника по пятницу с 09 до 20, в субботу с 09 до 18. При доставке за МКАД и по Московской области (с понедельника по пятницу с 09 до 18) стоимость доставки составит 290р. +50р. за каждые 5 км.
 					Стоимость самовывоза – 90р. Адреса самовывоза уточняйте у менеджера.
 					Стоимость доставки почтой – по тарифам Почты России. ВАЖНО: На почте есть определенные сроки хранения посылки. Убедительная просьба забирать ваши заказы вовремя!</p>
-					<p class="db-select-three">Оставьте заказ на нашем сайте, и вы получите подарочный набор камней Sheriff Premium любым удобным вам способом!</p>
-					<a href="#" class="df-link">Оформить заказ</a>
+					<span class="db-select-span">Оставьте заказ на нашем сайте, и вы получите подарочный набор камней Sheriff Premium любым удобным вам способом!</p>
+						<div class="db-a-container">
+							<a href="#" class="df-link db-select-a">Оформить заказ</a>
+						</div>
+					</div>
 				</div>
 			</div>
-		</div>
-	</section>
+		</section>
+		<p class="db-p-last">Остались вопросы? <a href="#">ЗАДАЙТЕ ИХ МЕНЕДЖЕРУ</a> или <a href="#">ЗАКАЖИТЕ ЗВОНОК</a></p>
