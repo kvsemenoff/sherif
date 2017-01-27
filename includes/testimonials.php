b49cc14207ed1e1a97b21822a7aa4541b868de5c
unknown <anzarsh@mail.ru>
2017-01-27 14:25:01 +0300

    add 1

--- includes/testimonials.php
+++ includes/testimonials.php
@@ -2,73 +2,99 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<h2 class="db-test-h2">Посмотрите отзывы довольных покупателей
+				<h2 class="db-test-h2">Посмотрите отзывы довольных покупателей <br>
 					о камнях Sheriff Premium</h2>
 				</div>
 				<div class="clearfix"></div>
-				<div class="col-md-6">
-
+				
+				<div class="db-otziv-block-one">
 					<div class="db-otziv-content">
 						<div class="db-otziv-content-one">
-							<img src="img/db-otziv-one.jpg" alt="img" class="img-responsive">
+							<img src="img/db-otziv-one.jpg" alt="img" class="db-img img-responsive">
+							<span>Оценка:</span>
 						</div>
 						<div class="db-otziv-content-two">
 							<p class="db-first"><span>Елена, </span>г. Москва</p>
 							<p class="db-second">Муж доволен подарком, крепкий виски долго сохраняется охлажденным. Покупала ему на день рождения, а сейчас пришла за вторым набором для свёкра. Надо налаживать отношения ))</p>
 						</div>
-<div class="clearfix"></div>
-						<div class="db-otziv-content-one">
-							<img src="img/db-otziv-two.jpg" alt="img" class="img-responsive">
-						</div>
-						<div class="db-otziv-content-two">
-							<p class="db-first"><span>Елена,</span>г. Москва</p>
-							<p class="db-second">Муж доволен подарком, крепкий виски долго сохраняется охлажденным. Покупала ему на день рождения, а сейчас пришла за вторым набором для свёкра. Надо налаживать отношения ))</p>
-						</div>
+						<div class="clearfix"></div>
 					</div>
 
-
-					<div class="db-otziv-content-one">
-							<img src="img/db-otziv-three.jpg" alt="img" class="img-responsive">
-						</div>
-						<div class="db-otziv-content-two">
-							<p class="db-first"><span>Елена,</span>г. Москва</p>
-							<p class="db-second">Муж доволен подарком, крепкий виски долго сохраняется охлажденным. Покупала ему на день рождения, а сейчас пришла за вторым набором для свёкра. Надо налаживать отношения ))</p>
-						</div>
-
-
-				</div>
-
-				<div class="col-md-6">
 
 					<div class="db-otziv-content">
 						<div class="db-otziv-content-one">
-							<img src="img/db-otziv-four.jpg" alt="img" class="img-responsive">
+							<img src="img/db-otziv-two.jpg" alt="img" class="img-responsive">
+								<span>Оценка:</span>
 						</div>
 						<div class="db-otziv-content-two">
-							<p class="db-first"><span>Елена,</span>г. Москва</p>
-							<p class="db-second">Муж доволен подарком, крепкий виски долго сохраняется охлажденным. Покупала ему на день рождения, а сейчас пришла за вторым набором для свёкра. Надо налаживать отношения ))</p>
+						<p class="db-first"><span>Татьяна, </span>г. Москва</p>
+							<p class="db-second">Давно хотела их купить на подарок мужчине своему. И вот купила )) Когда-нибудь у меня закончится беременно вскармливательный период моей жизни и я тоже позволю себе хороший виски с этими камушками ))</p>
 						</div>
+						<div class="clearfix"></div>
+					</div>
 
+					<div class="db-otziv-content">
 						<div class="db-otziv-content-one">
-							<img src="img/db-otziv-five.jpg" alt="img" class="img-responsive">
+							<img src="img/db-otziv-three.jpg" alt="img" class="img-responsive">
+								<span>Оценка:</span>
 						</div>
 						<div class="db-otziv-content-two">
-							<p class="db-first"><span>Елена,</span>г. Москва</p>
-							<p class="db-second">Муж доволен подарком, крепкий виски долго сохраняется охлажденным. Покупала ему на день рождения, а сейчас пришла за вторым набором для свёкра. Надо налаживать отношения ))</p>
+							<p class="db-first"><span>Александра, </span>г. Новосибирск</p>
+							<p class="db-second">Камни шериф достойны уважения и стоят тех денег, которые за них просят. Настоящий стеатит (проверено на себе), упаковка вне всяких похвал, дополнительно украшать подарок не нужно, выглядят отлично. И получился целый ритуал по разворачиванию и рассматриванию что там внутри. Так что девочки, не раздумывайте, вашему мужчине точно понравятся.</p>
 						</div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+
+
+				<div class="db-otziv-block-two">
+					<div class="db-otziv-content">
+						<div class="db-otziv-content-one">
+							<img src="img/db-otziv-four.jpg" alt="img" class="img-responsive">
+								<span>Оценка:</span>
+						</div>
+						<div class="db-otziv-content-two">
+							<p class="db-first"><span>Елена, </span>г. Ростов на Дону</p>
+							<p class="db-second">Крутой подарок, идея супер! Спасибо сестре, надоумила. Показала мне набор Шериф – выглядят очень круто, ничего не скажешь. Уверена, что моему мужчине понравятся! И пусть теперь он ломает голову, что подарить мне на д\р в ответку))</p>
+						</div>
+						<div class="clearfix"></div>
 					</div>
 
 
-					<div class="db-otziv-content-one">
-							<img src="img/db-otziv-six.jpg" alt="img" class="img-responsive">
+					<div class="db-otziv-content">
+						<div class="db-otziv-content-one">
+							<img src="img/db-otziv-five.jpg" alt="img" class="img-responsive">
+								<span>Оценка:</span>
 						</div>
 						<div class="db-otziv-content-two">
-							<p class="db-first"><span>Елена,</span>г. Москва</p>
+							<p class="db-first"><span>Елена, </span>г. Тверь</p>
+							<p class="db-second">Упакованы камешки, конечно, по высшему разряду. Выглядят супер! С гордостью дарила, а не то что «не стыдно подарить». Муж был очень удивлен, главное приятно удивлен. Говорит ожидал очередной галстук, а тут такое ))</p>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+
+					<div class="db-otziv-content">
+						<div class="db-otziv-content-one">
+							<img src="img/db-otziv-six.jpg" alt="img" class="img-responsive">
+								<span>Оценка:</span>
+						</div>
+						<div class="db-otziv-content-two">
+							<p class="db-first"><span>Елена, </span>г. Москва</p>
 							<p class="db-second">Муж доволен подарком, крепкий виски долго сохраняется охлажденным. Покупала ему на день рождения, а сейчас пришла за вторым набором для свёкра. Надо налаживать отношения ))</p>
 						</div>
-
-
+						<div class="clearfix"></div>
+					</div>
 				</div>
+				<div class="clearfix"></div>
+				<div class="db-down">
+<div class="col-md-6">
+	<a href="#" class="df-link db-link-test">ЗАКАЗАТЬ</a>
+</div>
+<div class="col-md-6">
+	<a href="#" class="db-link">ОСТАВИТЬ ОТЗЫВ</a>
+</div>
+</div>
+
 
 			</div>
 		</div>
