329f96866820429f5211989fc86fde02a4d9caca
engine77 <zaur.ka.00@mail.ru>
2017-01-27 12:11:30 +0300

    25

--- spasibo.php
+++ spasibo.php
@@ -6,7 +6,33 @@
 
 	<div class="n-content">
 		<div class="n-content-inner vertical-align">
-			
+			<div class="df-thankstovar">
+				<h2>Возможно вам это понравится!</h2>
+				<div class="df-tovar">
+					<div class="df-tovar__img">
+						<img src="img/df-tovar.jpg" alt="" class="img-responsive">
+					</div>
+					<div class="df-tovar__txt">
+						Отличные, качественные спортивные наушники! Не имеют проводов, ничего не будет путаться или болтаться перед глазами. Их можно быстро надеть или снять. <br>
+						Вы можете вставить карту памяти и слушать любимую музыку, либо просто включить радио. Качество звука на очень высоком уровне, прекрасно слышны как басы, так и высокие частоты.
+					</div>
+				</div>
+				<div class="df-tovar">
+					<div class="df-tovar__img">
+						<img src="img/df-tovar.jpg" alt="" class="img-responsive">
+					</div>
+					<div class="df-tovar__txt">
+						Отличные, качественные спортивные наушники! Не имеют проводов, ничего не будет путаться или болтаться перед глазами. Их можно быстро надеть или снять. <br>
+						Вы можете вставить карту памяти и слушать любимую музыку, либо просто включить радио. Качество звука на очень высоком уровне, прекрасно слышны как басы, так и высокие частоты.
+					</div>
+				</div>
+
+
+				<div class="df-butoon">
+					<a href="" class="df-butlink">some text</a>
+					<span class="df-imgbutoon"><img src="img/dfbgfon.png" alt="" ></span>
+				</div>
+			</div>
 		</div>
 	</div>
 
