af771d94d378fa187f82742c81a5fe92e7a5e0d5
engine77 <zaur.ka.00@mail.ru>
2017-01-31 15:10:59 +0300

    31

--- spasibo.php
+++ spasibo.php
@@ -10,30 +10,66 @@
 				<h2>Возможно вам это понравится!</h2>
 				<div class="df-tovar">
 					<div class="df-tovar__img">
-						<img src="img/df-viski.jpg" alt="" class="img-responsive">
+						<a target="_blank" href="http://market-4u.ru/device/syllable-d900mini.html?utm_source=partner&utm_medium=free&utm_campaign=sheriff
+"><img src="img/har10.jpg" alt="" class="img-responsive"></a>
 					</div>
 					<div class="df-tovar__txt">
-						Это действительно самая большая энциклопедия о виски – состоит из 960 страниц! В этой книге вы найдете:
-						историю возникновения этого напитка,
-						информацию обо всех производимых виски на территории Шотландии, Ирландии, США, Канады, Японии, Европе и др.
-						увлекательные описание производства виски со всеми тонкостями, благодаря которым получается такой благородный напиток,
-						и многое многое другое.
+						БЕСПРОВОДНЫЕ стерео наушники Syllable D900 mini - удивительно чистый звук и без проводов! <br>
+						Подходят для смартфонов на Android, iOs, Windows и т.д. До 24 часов работы при использовании зарядного футляра (идет в комплекте).
+					</div>
+				</div>
+				<div class="df-tovar last">
+					<div class="df-tovar__img">
+						<a target="_blank" href="http://market-4u.ru/device/fidgetcube.htmlhttp://market-4u.ru/device/syllable-d900mini.html?utm_source=partner&utm_medium=free&utm_campaign=sheriff
+"><img src="img/cube.jpg" alt="" class="img-responsive"></a>
+					</div>
+					<div class="df-tovar__txt">
+						Кубик-антистресс Fidget Cube - щёлкайте, крутите и клацайте, не отвлекая и не раздражая окружающих. <br>
+						Концентрирует внимание и улучшает продуктивность, поможет сосредоточиться уже через 5 минут использования. Оригинальный и бюджетный подарок другу или коллеге по любому поводу.							
+					</div>
+				</div>
+				
+				<div class="df-tovar">
+					<div class="df-tovar__img">
+						<a target="_blank" href="http://market-4u.ru/device/driversunglasses.html?utm_source=partner&utm_medium=free&utm_campaign=sheriff"><img src="img/glases.jpg" alt="" class="img-responsive"></a>
+					</div>
+					<div class="df-tovar__txt">
+						Универсальные очки для водителя Robesbon - 5 съёмных линз в комплекте! Безопасное вождение в любую погоду и время суток.
+						Блокируют ослепление от фар встречных автомобилей, уличных светильников, прожекторов и т.п. <br>
+					Уменьшают блики от гладких отражающих поверхностей. Улучшают видимость в ночное время, туман, дождь, снег.
+						
+					</div>
+				</div>
+				<div class="df-tovar last">
+					<div class="df-tovar__img">
+						<a target="_blank" href="http://market-4u.ru/fanclastic?utm_source=partner&utm_medium=free&utm_campaign=sheriff"><img src="img/har2.jpg" alt="" class="img-responsive"></a>
+					</div>
+					<div class="df-tovar__txt">
+						3д конструктор Фанкластик — невероятные модели своими руками!
+						Изобретен и производится в России. Можно строить любые 3Д-модели, на что хватит фантазии. Развивает пространственное воображение, мышление и фантазию.						
 					</div>
 				</div>
 				<div class="df-tovar">
 					<div class="df-tovar__img">
-						<img src="img/df-ears.png" alt="" class="img-responsive">
+						<a target="_blank" href="http://ant-farm.ru/ferma?utm_source=partner&utm_medium=free&utm_campaign=sheriff
+						"><img src="img/ferm.jpg" alt="" class="img-responsive"></a>
 					</div>
 					<div class="df-tovar__txt">
-						Отличные, качественные спортивные наушники! Не имеют проводов, ничего не будет путаться или болтаться перед глазами. Их можно быстро надеть или снять.		
-						Вы можете вставить карту памяти и слушать любимую музыку, либо просто включить радио. Качество звука на очень высоком уровне, прекрасно слышны как басы, так и высокие частоты.							
+						Живые муравьиные фермы – удивительный мир муравьев у вас на столе!
+						Наблюдайте за жизнью муравьёв в условиях, максимально приближенных к естественным. Прекрасное средство для борьбы со стрессом и усталостью. Оригинальный подарок детям и взрослым под любой бюджет и по любому поводу!
+							
 					</div>
 				</div>
-				
-				<!-- <div class="df-butn">				
-					<a href="" class="df-butlink">заказать </a>
-					<span class="df-imgbutoon"><img src="img/dfbgfon.png" alt="" class="img-responsive" ></span>				
-				</div> -->
+					<div class="df-tovar last">
+					<div class="df-tovar__img">
+						<a target="_blank" href="http://покемоны-тут.рф/?utm_source=partner&utm_medium=free&utm_campaign=sheriff"><img src="img/pika.jpg" alt="" class="img-responsive"></a>
+					</div>
+					<div class="df-tovar__txt">
+						Герои популярной игры в ваших руках!
+						Вы азартный ловец покемонов? Среди ваших знакомых есть фанаты легендарного мультфильма? Тогда совершенно точно вам нужно посетить этот магазин с огромным ассортиментом!							
+					</div>
+				</div>
+
 			</div>
 		</div>
 	</div>
